93a02703692d1d11bcd1603e902f99e72885ca56
IvanovDeveloper <ivanov.andrey.developer@gmail.com>
2019-01-10 01:43:57 -0800

    Add project source

---
+++ Steps/TestProject/Controller/Base/SPBaseNavigationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SPBaseNavigationViewController.swift
+//  TestProject
+//
+//  Created by Ivanov Developer on 1/10/19.
+//  Copyright © 2019 Andrew Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class SPBaseNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
---
+++ Steps/TestProject/Controller/Comments Settings/SPCommentsSettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SPComentsSettingsViewController.swift
+//  TestProject
+//
+//  Created by Ivanov Developer on 1/10/19.
+//  Copyright © 2019 Andrew Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class SPCommentsSettingsViewController: SPBaseViewController {
+
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
